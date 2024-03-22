4514f61816d37531ced9cabe81e580ee0d667a68
Radwano01 <127989522+Radwano01@users.noreply.github.com>
2024-03-22 03:01:19 +0300

    Update PackageController.java

--- src/main/java/com/hackathon/backend/Controllers/PackageController.java
+++ src/main/java/com/hackathon/backend/Controllers/PackageController.java
@@ -1,6 +1,5 @@
 package com.hackathon.backend.Controllers;
 
-
 import com.hackathon.backend.Dto.PackageDto.BenefitDto;
 import com.hackathon.backend.Dto.PackageDto.PackageDto;
 import com.hackathon.backend.Dto.PackageDto.PackageRoadmapDto;
@@ -11,7 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RequestMapping(path = "${package.controller.path}")
+@RequestMapping(path = "${PACKAGE_API_PATH}")
 public class PackageController {
 
     private final PackageService packageService;
@@ -20,89 +19,88 @@ public class PackageController {
         this.packageService = packageService;
     }
 
-    @PostMapping(path = "${package.create.package.path}")
+    @PostMapping(path = "${PACKAGE_CREATE_PACKAGE_PATH}")
     public ResponseEntity<?> createPackage(@RequestBody PackageDto packageDto){
         return packageService.createPackage(packageDto);
     }
 
-    @PostMapping(path = "${package.create.benefit.path}")
+    @PostMapping(path = "${PACKAGE_CREATE_BENEFIT_PATH}")
     public ResponseEntity<?> createBenefitForPackage(@RequestBody BenefitDto benefitDto){
         return packageService.createBenefitForPackage(benefitDto);
     }
 
-    @PostMapping(path = "${package.create.roadmap.path}")
+    @PostMapping(path = "${PACKAGE_CREATE_ROADMAP_PATH}")
     public ResponseEntity<?> createRoadmapForPackage(@RequestBody PackageRoadmapDto roadmapDto) {
         return packageService.createRoadmapForPackage(roadmapDto);
     }
 
-    @PostMapping(path = "${package.create.todo.path}")
+    @PostMapping(path = "${PACKAGE_CREATE_TODO_PATH}")
     public ResponseEntity<?> createTodoForPackage(@RequestBody PackageTodosDto todosDto){
         return packageService.createTodosForPackage(todosDto);
     }
 
-    @GetMapping(path = "${package.get.packages.path}")
+    @GetMapping(path = "${PACKAGE_GET_PACKAGES_PATH}")
     public ResponseEntity<?> getAllPackages(){
         return packageService.getAllPackages();
     }
 
-    @GetMapping(path = "${package.get.packages.from.country.path}")
+    @GetMapping(path = "${PACKAGE_GET_PACKAGES_FROM_COUNTRY_PATH}")
     public ResponseEntity<?> getAllPackagesFromCountry(@RequestBody PackageDto packageDto){
         return packageService.getAllPackagesFromCountry(packageDto);
     }
 
-    @GetMapping(path = "${package.get.benefits.path}")
+    @GetMapping(path = "${PACKAGE_GET_BENEFITS_PATH}")
     public ResponseEntity<?> getAllBenefit(){
         return packageService.getAllBenefit();
     }
 
-    @GetMapping(path = "${package.get.roadmaps.path}")
+    @GetMapping(path = "${PACKAGE_GET_ROADMAPS_PATH}")
     public ResponseEntity<?> getAllRoadmapsFromCountry(){
         return packageService.getAllRoadmap();
     }
 
-    @GetMapping(path = "${package.get.todos.path}")
+    @GetMapping(path = "${PACKAGE_GET_TODOS_PATH}")
     public ResponseEntity<?> getAllTodosFromCountry(){
         return packageService.getAllTodos();
     }
 
-    @DeleteMapping(path = "${package.delete.package.path}")
+    @DeleteMapping(path = "${PACKAGE_DELETE_PACKAGE_PATH}")
     public ResponseEntity<?> deletePackage(@PathVariable int id){
         return packageService.deletePackage(id);
     }
 
-    @DeleteMapping(path = "${package.delete.benefit.path}")
+    @DeleteMapping(path = "${PACKAGE_DELETE_BENEFIT_PATH}")
     public ResponseEntity<?> deleteBenefit(@PathVariable int id){
         return packageService.deleteBenefit(id);
     }
 
-    @DeleteMapping(path = "${package.delete.roadmap.path}")
+    @DeleteMapping(path = "${PACKAGE_DELETE_ROADMAP_PATH}")
     public ResponseEntity<?> deleteRoadmap(@PathVariable("id") int id){
         return packageService.deleteRoadmap(id);
     }
 
-    @DeleteMapping(path = "${package.delete.todo.path}")
+    @DeleteMapping(path = "${PACKAGE_DELETE_TODO_PATH}")
     public ResponseEntity<?> deleteTodo(@PathVariable("id") int id){
         return packageService.deleteTodos(id);
     }
 
-    @PutMapping(path = "${package.edit.package.path}")
+    @PutMapping(path = "${PACKAGE_EDIT_PACKAGE_PATH}")
     public ResponseEntity<?> editPackage(@PathVariable int id, @RequestBody PackageDto packageDto){
         return packageService.editPackage(id,packageDto);
     }
 
-    @PutMapping(path = "${package.edit.benefit.path}")
+    @PutMapping(path = "${PACKAGE_EDIT_BENEFIT_PATH}")
     public ResponseEntity<?> editBenefit(@PathVariable int id, @RequestBody BenefitDto benefitDto){
         return packageService.editBenefit(id, benefitDto);
     }
 
-    @PutMapping(path = "${package.edit.roadmap.path}")
+    @PutMapping(path = "${PACKAGE_EDIT_ROADMAP_PATH}")
     public ResponseEntity<?> editRoadmap(@PathVariable("id") int id,@RequestBody PackageRoadmapDto roadmapDto){
         return packageService.editRoadmap(id, roadmapDto);
     }
 
-    @PutMapping(path = "${package.edit.todo.path}")
+    @PutMapping(path = "${PACKAGE_EDIT_TODO_PATH}")
     public ResponseEntity<?> editTodo(@PathVariable("id") int id,@RequestBody PackageTodosDto todosDto){
         return packageService.editTodos(id, todosDto);
     }
 }
-
