9519dcb6aeebf76aa780185291fea9d08de976cf
Radwano01 <127989522+Radwano01@users.noreply.github.com>
2024-03-25 20:27:18 +0300

    Update BackendApplication.java

--- src/main/java/com/hackathon/backend/BackendApplication.java
+++ src/main/java/com/hackathon/backend/BackendApplication.java
@@ -2,15 +2,10 @@ package com.hackathon.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
-@ComponentScan("com.hackathon.backend.Config")
+@SpringBootApplication
 public class BackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
-
-
 }
